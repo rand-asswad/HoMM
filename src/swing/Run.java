e808205f3537535277e2a654bbd2a3bff22be7a5
Rand Asswad <rand.asswad@insa-rouen.fr>
2018-01-07 18:43:47 +0100

    minor change

--- src/swing/Run.java
+++ src/swing/Run.java
@@ -1,13 +1,7 @@
 package swing;
-import com.bulenkov.darcula.DarculaLaf;
-import model.Game;
-
-import javax.swing.*;
 
 public class Run {
-    public static void main(String [] args) throws Exception {
-        UIManager.setLookAndFeel(new DarculaLaf());
-        JFrame menu = new MainMenu();
-        menu.setVisible(true);
+    public static void main(String [] args) {
+        new MainMenu().setVisible(true);
     }
 }
